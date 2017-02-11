894acd328081c25386303fa22bb19f2af9a567ac
acjam113 <acjam113@gmail.com>
2017-02-10 22:07:14 -0800

    updated version of filmActor helper and controller

--- src/java/dvdrental/FilmActorController.java
+++ src/java/dvdrental/FilmActorController.java
@@ -4,7 +4,6 @@ package dvdrental;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
-import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.Date;
 import javax.faces.model.DataModel;
@@ -25,6 +24,16 @@ public class FilmActorController implements Serializable {
     
     // this is our helper class that uses hibernate to query the database
     FilmActorHelper helper;
+    
+    // these fields map to components in the film.xhtml
+    // they represent the actual values input and selected by the user
+    String title;
+    String description;
+    int actor;
+    String rating;
+    int category;
+    int language;
+    String response;
 
     /**
      * Creates a new instance of FilmActorController
@@ -66,6 +75,85 @@ public class FilmActorController implements Serializable {
     public void setLanguageValues(DataModel languageValues) {
         this.languageValues = languageValues;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getActor() {
+        return actor;
+    }
+
+    public void setActor(int actor) {
+        this.actor = actor;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public int getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(int language) {
+        this.language = language;
+    }
+
+    public String getResponse() {
+        
+        if (title != null && description != null){
+            
+            Date date = new Date();
+            Timestamp timeStamp = new Timestamp(date.getTime());
+            
+            if(helper.insert(title, description, actor, category, language, rating, timeStamp) == 1){
+                title = null;
+                description = null;
+                response = "Film Added.";
+                return response;
+            } else {
+                title = null;
+                description = null;
+                response = "Film Not Added.";
+                return response;
+            } 
+        } else {
+                response = " ";
+                return response;
+        }
+       
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+    
+    
     
     
     
--- src/java/dvdrental/FilmActorHelper.java
+++ src/java/dvdrental/FilmActorHelper.java
@@ -100,4 +100,160 @@ public class FilmActorHelper {
         
         return languageList;
     }
+    
+    // this method is going to insert a film into the film table
+    private int insertFilm(String title, String description, int language,
+            String rating, Timestamp timeStamp){
+        
+        int result = 0;
+        
+        String sql = "insert into film "
+                + "(title, description, language_id, rental_duration, rental_rate, "
+                + "replacement_cost, rating, last_update) "
+                + "values (:title, :description, :languageId, :rentalDuration, "
+                + ":rentalRate, :replacementCost, :rating, :update)";
+        
+        try{
+            // if the current transaction isn't active, begin one
+            if(!this.session.getTransaction().isActive()){
+                session.beginTransaction();
+            }
+            
+            // creating actual query that will be executed against the database
+            SQLQuery q = session.createSQLQuery(sql);
+            
+            q.addEntity(Film.class);
+            
+            q.setParameter("title", title);
+            q.setParameter("description", description);
+            q.setParameter("languageId", language);
+            q.setParameter("rentalDuration", 3);
+            q.setParameter("rentalRate", 4.99);
+            q.setParameter("replacementCost", 19.99);
+            q.setParameter("rating", rating);
+            q.setParameter("update", timeStamp);
+            
+            result = q.executeUpdate();
+            
+            session.getTransaction().commit();
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        
+        return result;
+    }
+    
+    // this method is going to return the film id of the last film inserted
+    // into the film table
+    private int getFilmId(){
+        
+        List<Film> filmList = null;
+        
+        String sql = "select * from film order by last_update desc limit 1";
+        
+        try{
+            // if the current transaction isn't active, begin one
+            if(!this.session.getTransaction().isActive()){
+                session.beginTransaction();
+            }
+            
+            // creating actual query that will be executed against the database
+            SQLQuery q = session.createSQLQuery(sql);
+            
+            q.addEntity(Film.class);
+            
+            filmList = (List<Film>) q.list();
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        return filmList.get(0).getFilmId();
+    }
+    
+    // this method is going to insert a film actor into the film actor table
+    private int insertFilmActor(int actor, int film, Timestamp timeStamp){
+        int result = 0;
+        
+        String sql = "insert into film_actor values (:actorId, :filmId, :update)";
+        
+        try{
+            // if the current transaction isn't active, begin one
+            if(!this.session.getTransaction().isActive()){
+                session.beginTransaction();
+            }
+            
+            // creating actual query that will be executed against the database
+            SQLQuery q = session.createSQLQuery(sql);
+            
+            q.addEntity(FilmActor.class);
+            
+            q.setParameter("actorId", actor);
+            q.setParameter("filmId", film);
+            q.setParameter("update", timeStamp);
+            
+            result = q.executeUpdate();
+            
+            session.getTransaction().commit();
+            
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        return result;
+    }
+    
+    // this method is going to insert a film category into the film category table
+    private int insertFilmCategory(int film, int category, Timestamp timeStamp){
+        
+        int result = 0;
+        
+        String sql = "insert into film_category values (:filmId, :categoryId, :update)";
+        
+        try{
+            // if the current transaction isn't active, begin one
+            if(!this.session.getTransaction().isActive()){
+                session.beginTransaction();
+            }
+            
+            // creating actual query that will be executed against the database
+            SQLQuery q = session.createSQLQuery(sql);
+            
+            q.addEntity(FilmCategory.class);
+            
+            q.setParameter("categoryId", category);
+            q.setParameter("filmId", film);
+            q.setParameter("update", timeStamp);
+            
+            result = q.executeUpdate();
+            
+            session.getTransaction().commit();
+            
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        
+        return result;
+    }
+    
+    public int insert (String title, String description, int actor, int category,
+            int language, String rating, Timestamp timeStamp){
+        
+        int result = 0;
+        
+        int filmResult = insertFilm(title, description, language, rating, timeStamp);
+        int filmId = getFilmId();
+        int actorResult = insertFilmActor(actor, filmId, timeStamp);
+        int categoryResult = insertFilmCategory(filmId, category, timeStamp);
+        
+        if(filmResult == 1 && actorResult == 1 && categoryResult == 1){
+            result = 1;
+        }
+        
+        return result;
+    }
 }
